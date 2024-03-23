abe8942c521a84763ba19117ed158ba0c82ef45a
madeleineberild <madeleine.berild@hotmail.com>
2024-03-23 10:27:13 +0100

    Setup something for xcode that maybe worked idk

---
+++ kattis/kattis/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kattis
+//
+//  Created by Madeleine Berild on 2024-03-23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // this is where i could write things i presume
+    std::cout << "Hello, World!\n" << std::endl;
+    return 0;
+}
